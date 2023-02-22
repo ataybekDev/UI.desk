6d6c01aa2292ebc95ab5368bd746e7ae3ebbcef4
ataybekDev <ataiakimov2003@gmail.com>
2023-02-22 20:47:40 +0600

    initial

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    loop:true,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+    },
+});
